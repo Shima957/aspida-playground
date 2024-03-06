635e226953f1b52d47eb83852a7d1cb215f60a58
Shima957 <Shima957@users.noreply.github.com>
2024-03-06 15:32:02 +0000

    generate api type

--- api/$api.ts
+++ api/$api.ts
@@ -1,24 +1,45 @@
 import type { AspidaClient, BasicHeaders } from 'aspida';
+import type { Methods as Methods_zapyl } from './book/_bookId';
 import type { Methods as Methods_1swob1w } from './books';
 
 const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
   const prefix = (baseURL === undefined ? 'http://localhost:3000' : baseURL).replace(/\/$/, '');
-  const PATH0 = '/books';
+  const PATH0 = '/book';
+  const PATH1 = '/books';
   const GET = 'GET';
 
   return {
+    book: {
+      _bookId: (val1: number | string) => {
+        const prefix1 = `${PATH0}/${val1}`;
+
+        return {
+          /**
+           * @returns OK
+           */
+          get: (option?: { config?: T | undefined } | undefined) =>
+            fetch<Methods_zapyl['get']['resBody'], BasicHeaders, Methods_zapyl['get']['status']>(prefix, prefix1, GET, option).json(),
+          /**
+           * @returns OK
+           */
+          $get: (option?: { config?: T | undefined } | undefined) =>
+            fetch<Methods_zapyl['get']['resBody'], BasicHeaders, Methods_zapyl['get']['status']>(prefix, prefix1, GET, option).json().then(r => r.body),
+          $path: () => `${prefix}${prefix1}`,
+        };
+      },
+    },
     books: {
       /**
        * @returns OK
        */
       get: (option?: { config?: T | undefined } | undefined) =>
-        fetch<Methods_1swob1w['get']['resBody'], BasicHeaders, Methods_1swob1w['get']['status']>(prefix, PATH0, GET, option).json(),
+        fetch<Methods_1swob1w['get']['resBody'], BasicHeaders, Methods_1swob1w['get']['status']>(prefix, PATH1, GET, option).json(),
       /**
        * @returns OK
        */
       $get: (option?: { config?: T | undefined } | undefined) =>
-        fetch<Methods_1swob1w['get']['resBody'], BasicHeaders, Methods_1swob1w['get']['status']>(prefix, PATH0, GET, option).json().then(r => r.body),
-      $path: () => `${prefix}${PATH0}`,
+        fetch<Methods_1swob1w['get']['resBody'], BasicHeaders, Methods_1swob1w['get']['status']>(prefix, PATH1, GET, option).json().then(r => r.body),
+      $path: () => `${prefix}${PATH1}`,
     },
   };
 };
---
+++ api/book/_bookId/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export type Methods = {
+  get: {
+    status: 200
+
+    /** OK */
+    resBody: {
+      id: string
+      title: string
+      isRead: boolean
+    }
+  }
+}
